b0c5ab546cf89878d4a1fb420f68e1bbb8d44263
Nour <nourahmedelgeziry@gmail.com>
2021-09-22 15:46:15 +0100

    completed requirement 4, filtered datas according to provided lease start date boundaries

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import pprint
+import pprint #used for requirement 3
+import datetime #used for requirement 4
+
+
 # read csv file
 import csv
 # read csv file and convert value to float and append list
 with open('Python Developer Test Dataset.csv', 'r') as file:
     reader = csv.DictReader(file)
-    '''
+    
     #requirenment 1
+
     rentList = list()
     for row in reader:
         rentList.append(float(row['Current Rent']))
@@ -21,6 +25,7 @@ with open('Python Developer Test Dataset.csv', 'r') as file:
         count += 1
     
     #requirement 2
+
     rent = list()
     #get rows where lease years = 25
     wantedLease = [row for row in reader if int(row['Lease Years']) == 25 ]
@@ -31,28 +36,41 @@ with open('Python Developer Test Dataset.csv', 'r') as file:
     #calculate the total rent
     totalRent = sum(rent)
     print(totalRent)   
-'''
+
     #requirement 3 
+
     tenantMastCount = {}
     #iterate through tenant names
-    
     for row in reader:
-        if row['Tenant Name'] in tenantMastCount:
-             #if alredy in dic , increase count
+         #if alredy in dic , increase count
+        if row['Tenant Name'] in tenantMastCount:   
             tenantMastCount[row['Tenant Name']] += 1
             #else initialize key
         else: tenantMastCount[row['Tenant Name']] = 1
 
+    #Printing 
     #using pprint library
-    #pprint.pprint(tenantMastCount)
+    pprint.pprint(tenantMastCount)
     #iterating through key:value pairs
     print("Tenant mast count:")
     for name, count in tenantMastCount.items():  # dct.iteritems() in Python 2
         print("{} ({})".format(name, count))
         
 
+    #requirement 4
 
-
+    #set the bhoundaries for the lease start date
+    lowerBound = datetime.datetime(1999, 6, 1)
+    upperBound = datetime.datetime(2007, 8, 31)
+    
+    #filter data according to boundaries by converting date strings to datetime fields
+    wantedRentalDates = [row for row in reader if datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') >lowerBound and datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') < upperBound ]
+    
+    #print the data
+    for ele in wantedRentalDates:
+        print('\n')
+        print(ele)
+        
     
     
        
