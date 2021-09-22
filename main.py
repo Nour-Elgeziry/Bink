f9c15455c779b10da2462980724a9bf7e9de590a
Nour <nourahmedelgeziry@gmail.com>
2021-09-22 14:20:13 +0100

    Completed rewuirement 3 where tenant name and mast count werer stored in a dict as key value pairs. Also printed the dic to connsole using two ways 1. using pprint library, 2.by iterating through the key:value pairs

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import pprint
 # read csv file
 import csv
 # read csv file and convert value to float and append list
 with open('Python Developer Test Dataset.csv', 'r') as file:
     reader = csv.DictReader(file)
-    #requirnment 1
-    
+    '''
+    #requirenment 1
     rentList = list()
     for row in reader:
         rentList.append(float(row['Current Rent']))
@@ -19,7 +20,7 @@ with open('Python Developer Test Dataset.csv', 'r') as file:
         print(val)
         count += 1
     
-    #requirnment 2
+    #requirement 2
     rent = list()
     #get rows where lease years = 25
     wantedLease = [row for row in reader if int(row['Lease Years']) == 25 ]
@@ -29,8 +30,30 @@ with open('Python Developer Test Dataset.csv', 'r') as file:
         rent.append(float(row['Current Rent']))
     #calculate the total rent
     totalRent = sum(rent)
-    print(totalRent)    
+    print(totalRent)   
+'''
+    #requirement 3 
+    tenantMastCount = {}
+    #iterate through tenant names
+    
+    for row in reader:
+        if row['Tenant Name'] in tenantMastCount:
+             #if alredy in dic , increase count
+            tenantMastCount[row['Tenant Name']] += 1
+            #else initialize key
+        else: tenantMastCount[row['Tenant Name']] = 1
 
+    #using pprint library
+    #pprint.pprint(tenantMastCount)
+    #iterating through key:value pairs
+    print("Tenant mast count:")
+    for name, count in tenantMastCount.items():  # dct.iteritems() in Python 2
+        print("{} ({})".format(name, count))
+        
+
+
+
+    
     
        
 
@@ -40,4 +63,3 @@ with open('Python Developer Test Dataset.csv', 'r') as file:
         
 
         
-``
