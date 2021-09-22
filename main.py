fe046947ce9a3be9b9f494dad79acf196ee74589
Nour <nourahmedelgeziry@gmail.com>
2021-09-22 20:18:06 +0100

    Improved how userinput is processed to chose which function to perform.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import datetime #used for requirement 4
 
 # read csv file
 import csv
+#requirenment 1
 def req1():
     # read csv file and convert value to float and append list
     with open('Python Developer Test Dataset.csv', 'r') as file:
         reader = csv.DictReader(file)
         
-        #requirenment 1
         rentList = list()
         for row in reader:
             rentList.append(float(row['Current Rent']))
@@ -23,11 +23,12 @@ def req1():
                 break
             print(val)
             count += 1
-        
+
+#requirement 2   
 def req2():
     with open('Python Developer Test Dataset.csv', 'r') as file:
         reader = csv.DictReader(file)
-        #requirement 2
+        
         rent = list()
         #get rows where lease years = 25
         wantedLease = [row for row in reader if int(row['Lease Years']) == 25 ]
@@ -41,11 +42,11 @@ def req2():
         print('\n')
         print('Total Rent = ', totalRent)   
         
-    
+#requirement 3
 def req3():
     with open('Python Developer Test Dataset.csv', 'r') as file:
         reader = csv.DictReader(file)
-        #requirement 3 
+         
         tenantMastCount = {}
         #iterate through tenant names
         for row in reader:
@@ -62,11 +63,12 @@ def req3():
         print("Tenant mast count:")
         for name, count in tenantMastCount.items():  # dct.iteritems() in Python 2
             print("{} ({})".format(name, count))
-    
+
+#requirement 4    
 def req4():
     with open('Python Developer Test Dataset.csv', 'r') as file:
         reader = csv.DictReader(file)
-        #requirement 4
+        
         #set the bhoundaries for the lease start date
         lowerBound = datetime.datetime(1999, 6, 1)
         upperBound = datetime.datetime(2007, 8, 31)
@@ -78,27 +80,23 @@ def req4():
         for ele in wantedRentalDates:
             print('\n')
             print(ele)
-    
+#all requirements
+def allReq():
+    req1()
+    req2()
+    req3()
+    req4()
+
 #if __name__ == '__main__':
+#dictionary to hold key value pairs of finctions to perform depending on user input
+func_dict = {'1':req1, '2':req2, '3':req3, '4':req4, '5': allReq}
 userInput = input('Enter 1,2,3,4 to perform one of the operations or Enter 5 to perform all operations: ')
-
 try:
+    #check if userinput is a number
     value = int(userInput)
-    if value == 5:
-        req1()
-        req2()
-        req3()
-        req4()
-    elif value == 1:
-        req1()
-    elif value == 2:
-        req2()
-    elif value == 3:
-        req3()
-    elif value == 4:
-        req4()
-    else: print('Entered number out of range, please rerun program and choose a number between 1 and 5')
-
+    if value in range(1,6):
+        func_dict[userInput]()
+    else: print('Entered number out of range, please reload program and choose a number between 1 and 5')
 except:print('Please reload the program and enter a number')
 
 
