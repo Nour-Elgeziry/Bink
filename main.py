eaee4abcb98e47f021811eb6544670ffd3246360
Nour <nourahmedelgeziry@gmail.com>
2021-09-23 09:36:06 +0100

    Optimized each individual function as well as runnig the functions separatly and all together depending on user input

--- main.py
+++ main.py
@@ -1,114 +1,70 @@
-import pprint #used for requirement 3
+import json
 import datetime #used for requirement 4
-
-
 # read csv file
 import csv
 #requirenment 1
-def req1():
-    # read csv file and convert value to float and append list
-    with open('Python Developer Test Dataset.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        #add dict to array to enable iteration
-        data = [r for r in reader]
-        #sort the list
-        data.sort(key=lambda r:float(r['Current Rent']))
-        print(data[:5])
+def req1(data):
+    #sort the list
+    data.sort(key=lambda r:float(r['Current Rent']))
+    return data[:5] 
 
-        
-        
-req1()
-'''
 #requirement 2   
-def req2():
-    with open('Python Developer Test Dataset.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        
-        rent = list()
-        #get rows where lease years = 25
-        wantedLease = [row for row in reader if int(row['Lease Years']) == 25 ]
-        for row in wantedLease:
-            print('\n')
-            print(row)
-            #append rent values to rent list as floats
-            rent.append(float(row['Current Rent']))
-        #calculate the total rent
-        totalRent = sum(rent)
-        print('\n')
-        print('Total Rent = ', totalRent)   
-        
+def req2(data):    
+    #get rows where lease years = 25
+    wantedLease = [row for row in data if int(row['Lease Years']) == 25 ]
+    totalRent = 0
+    for row in wantedLease:
+        #append rent values to rent list as floats
+        totalRent += float(row['Current Rent'])
+    #calculate the total rent
+    result = dict()
+    result['Total Rent'] = totalRent
+    result['Wanted Lease'] = wantedLease
+    return result
+          
 #requirement 3
-def req3():
-    with open('Python Developer Test Dataset.csv', 'r') as file:
-        reader = csv.DictReader(file)
-         
-        tenantMastCount = {}
-        #iterate through tenant names
-        for row in reader:
-            #if alredy in dic , increase count
-            if row['Tenant Name'] in tenantMastCount:   
-                tenantMastCount[row['Tenant Name']] += 1
-                #else initialize key
-            else: tenantMastCount[row['Tenant Name']] = 1
-
-        #Printing 
-        #using pprint library
-        #pprint.pprint(tenantMastCount)
-        #iterating through key:value pairs
-        print("Tenant mast count:")
-        for name, count in tenantMastCount.items():  # dct.iteritems() in Python 2
-            print("{} ({})".format(name, count))
+def req3(data):    
+    tenantMastCount = dict()
+    #iterate through tenant names
+    for row in data:
+        #if not in dic , init
+        if not row['Tenant Name'] in tenantMastCount: 
+            tenantMastCount[row['Tenant Name']] = 0
+        tenantMastCount[row['Tenant Name']] += 1   
+    #Printing 
+    print("Tenant mast count:")
+    return tenantMastCount
 
 #requirement 4    
-def req4():
-    with open('Python Developer Test Dataset.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        
-        #set the bhoundaries for the lease start date
-        lowerBound = datetime.datetime(1999, 6, 1)
-        upperBound = datetime.datetime(2007, 8, 31)
-        
-        #filter data according to boundaries by converting date strings to datetime fields
-        wantedRentalDates = [row for row in reader if datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') >lowerBound and datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') < upperBound ]
-        
-        #print the data
-        for ele in wantedRentalDates:
-            print('\n')
-            print(ele)
+def req4(data):
+    #set the bhoundaries for the lease start date
+    lowerBound = datetime.datetime(1999, 6, 1)
+    upperBound = datetime.datetime(2007, 8, 31)
+    #filter data according to boundaries by converting date strings to datetime fields
+    wantedRentalDates = [row for row in data
+        if datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') >lowerBound and datetime.datetime.strptime(row['Lease Start Date'], '%d %b %Y') < upperBound ]
+    #print the data
+    return wantedRentalDates
+
 #all requirements
-def allReq():
-    req1()
-    req2()
-    req3()
-    req4()
-
-#if __name__ == '__main__':
+def allReq(data):
+    result = dict()
+    for key,func in func_dict.items():
+        if key != '5': 
+            result[key] = func(data)
+    return result
 #dictionary to hold key value pairs of finctions to perform depending on user input
-'''
+func_dict = {'1':req1, '2':req2, '3':req3, '4':req4, '5': allReq}
+def run(name, data):
+    print(json.dumps(func_dict[name](data), indent=4))
 
-'''func_dict = {'1':req1, '2':req2, '3':req3, '4':req4, '5': allReq}
 userInput = input('Enter 1,2,3,4 to perform one of the operations or Enter 5 to perform all operations: ')
 try:
-    value = int(userInput) #check if userinput is a number
-    if value in range(1,6):
-        func_dict[userInput]()
-    else: print('Entered number out of range, please reload program and choose a number between 1 and 5')
+    value = userInput #check if userinput is a number
+    with open('Python Developer Test Dataset.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+        if value in func_dict.keys():
+            run(value, data)           
+        else: print('Entered number out of range, please reload program and choose a number between 1 and 5')
 except:print('Please reload the program and enter a number')
-'''
-
-
-
-
-
-
-    
-    
-    
-       
-
-    
-
-     
-        
-
-        
